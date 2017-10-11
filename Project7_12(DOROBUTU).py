2ab535390e0595e5330bee34c173db54632a4810
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-12 02:34:46 +0300

    Update Project7_12(DOROBUTU).py

--- Project7_12(DOROBUTU).py
+++ Project7_12(DOROBUTU).py
@@ -5,7 +5,8 @@ a = list(input("Введіть речення : "))
 i = 0 
 while i < len(a):
 	if a[i] == " " and a[i + 1] == " ":
-		a.pop(i+1)
+		while a[i+1]==" ":
+			a.pop(i+1)
 	i = i + 1
 k = ''.join(a)
 print(k)
