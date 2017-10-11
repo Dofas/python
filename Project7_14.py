f4f66488b7f4a6a747fc71eaa1c31a7d15733a18
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-12 02:24:24 +0300

    Add files via upload

---
+++ Project7_14.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python3
+import re
+
+
+def check(text):
+    if not text.strip():
+        return True
+
+    return re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b', text) is not None
+
+s=input('Vvedit6 email ')
+
+print(check(s))
+
