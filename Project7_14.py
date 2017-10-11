c029159910ee79fa0d73a12100b9afe4a338516a
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-12 02:25:33 +0300

    Update Project7_14.py

--- Project7_14.py
+++ Project7_14.py
@@ -6,7 +6,7 @@ def check(text):
     if not text.strip():
         return True
 
-    return re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,6}\b', text) is not None
+    return re.search(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}\b', text) is not None
 
 s=input('Vvedit6 email ')
 
