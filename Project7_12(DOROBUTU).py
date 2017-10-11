a6637a0fb0b2866a37d072912372a23aaa31b1b5
Dofas <32168254+Dofas@users.noreply.github.com>
2017-10-12 01:01:42 +0300

    Add files via upload

---
+++ Project7_12(DOROBUTU).py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+
+a = list(input("Введіть речення : "))
+
+i = 0 
+while i < len(a):
+	if a[i] == " " and a[i + 1] == " ":
+		a.pop(i+1)
+	i = i + 1
+k = ''.join(a)
+print(k)
